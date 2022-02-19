955b77b84ebee3c4cf79e0877c2d8172b473dce1
Ivan Fedorenko <ivan.r.fedorenko@gmail.com>
2022-02-19 12:47:09 +0200

    slider

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.single-item-rtl').slick({
+        rtl: true,
+        accessibility: true,
+        adaptiveHeight: true,
+        arrows: true,
+        dots: true,
+        dotsClass: "my dots",
+        mobileFirst: true
+    });
+  });
