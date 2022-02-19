a80d87b35bd747972770d218d2bc0bbe00541d25
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2022-02-19 11:15:17 +0200

    list is not working

--- js/change_color.js
+++ js/change_color.js
@@ -19,3 +19,25 @@ document.querySelector('#submit').addEventListener("click", () => {
   document.querySelector('#setDfltBack').addEventListener('click', () =>{
     document.body.style.background = `#BF6D30`;
   })
+
+  document.querySelector('#submit_2time').addEventListener("click", () => {
+    const hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+    let hexcode1 = "";
+    let hexcode2 = "";
+    let random_index = 0;
+    
+    for(let i = 0; i < 6;i++){
+      random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcode1 += hex_numbers[random_index];
+      random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcode2 += hex_numbers[random_index];
+    }
+  
+    document.body.style.background = `linear-gradient(to right, #${hexcode1}, #${hexcode2})`;
+   
+  });
+
+
+  document.querySelector('#setDfltBack_2time').addEventListener('click', () =>{
+    document.body.style.background = `#BF6D30`;
+  })
--- js/script.js
+++ js/script.js
@@ -1,3 +1,100 @@
+// document.querySelector("#pencil").addEventListener("click", () => {
+//   document.querySelector("#all_items").textContent = "";
+// })
+
+// document.querySelector("#user_input").addEventListener("keydown", (event) => {
+//   if(event.key == "Enter")
+//     addItem();
+// });
+
+// addItem = () => {
+//   const div_item = document.createElement("div")
+  
+
+
+
+//   const item = document.createElement("li")
+//   item.id = "li_item_list";
+
+//   const img1 = document.createElement("img")
+// img1.src = './images/bin.png'
+// img1.className = "item_img_of_list1"
+// img1.id = "first_img_in_list"
+
+// const img2 = document.createElement("img")
+// img2.src = './images/money.png'
+// img2.className = "item_img_of_list2"
+// img2.id = "second_img_in_list"
+
+// const img3 = document.createElement("img")
+// img3.src = './images/shopping-bag.png'
+// img3.className = "item_img_of_list3"
+// img3.id = "third_img_in_list"
+
+
+// // document.querySelector("#first_img_in_list").addEventListener("click", () => {
+// //   document.querySelector("#all_items").textContent = "";
+// // })
+
+
+
+  
+  
+//   item.textContent =   "- " + document.querySelector("#user_input").value;
+ 
+  
+//   item.addEventListener("click", () => {
+//     if(item.style.textDecoration != "line-through")
+//       item.style.textDecoration = "line-through";
+//     else
+//       item.style.textDecoration = "none";
+//   })
+
+//    document.querySelector("#all_items").appendChild(item);
+//   // document.querySelector('#unique_id').appendChild(div_item);
+ 
+//   document.querySelector("#all_items").appendChild(img1);
+//   document.querySelector("#all_items").appendChild(img2);
+//   document.querySelector("#all_items").appendChild(img3);
+//   document.querySelector("#user_input").value = "";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector("#pencil").addEventListener("click", () => {
   document.querySelector("#all_items").textContent = "";
 })
@@ -8,28 +105,50 @@ document.querySelector("#user_input").addEventListener("keydown", (event) => {
 });
 
 addItem = () => {
-  const div_item = document.createElement("div")
+    const user_insert = document.querySelector('#user_input').value
+
+
+
+
+
+    
+
+
+
+
+
+    const imageObject = {
+      src_bin: './images/bin.png',
+      class_bin: 'item_img_of_list1',
+      li_contains: 'list_contains',
+      div_class: 'div_class',
+      money_src: './images/money.png',
+      bag_src: './images/shopping-bag.png',
+
+    }
   
+    const li_item = `<li class = ${imageObject.li_contains}>- ${user_insert} <div class = ${imageObject.div_class}><img class = ${imageObject.class_bin} src = ${imageObject.src_bin}> <img class = ${imageObject.class_bin} src = ${imageObject.money_src}> <img class = ${imageObject.class_bin} src = ${imageObject.bag_src}></div></li>`
+    
 
 
 
-  const item = document.createElement("li")
-  item.id = "li_item_list";
+  //   const item = document.createElement("li")
+  //   item.id = "li_item_list";
 
-  const img1 = document.createElement("img")
-img1.src = './images/bin.png'
-img1.className = "item_img_of_list1"
-img1.id = "first_img_in_list"
+    const img1 = document.createElement("img")
+  img1.src = './images/bin.png'
+  img1.className = "item_img_of_list1"
+  img1.id = "first_img_in_list"
 
-const img2 = document.createElement("img")
-img2.src = './images/money.png'
-img2.className = "item_img_of_list2"
-img2.id = "second_img_in_list"
+  const img2 = document.createElement("img")
+  img2.src = './images/money.png'
+  img2.className = "item_img_of_list2"
+  img2.id = "second_img_in_list"
 
-const img3 = document.createElement("img")
-img3.src = './images/shopping-bag.png'
-img3.className = "item_img_of_list3"
-img3.id = "third_img_in_list"
+  const img3 = document.createElement("img")
+  img3.src = './images/shopping-bag.png'
+  img3.className = "item_img_of_list3"
+  img3.id = "third_img_in_list"
 
 
 // document.querySelector("#first_img_in_list").addEventListener("click", () => {
@@ -39,25 +158,31 @@ img3.id = "third_img_in_list"
 
 
   
-  
-  item.textContent =   "- " + document.querySelector("#user_input").value;
- 
-  
-  item.addEventListener("click", () => {
-    if(item.style.textDecoration != "line-through")
-      item.style.textDecoration = "line-through";
-    else
-      item.style.textDecoration = "none";
+  const list_of_all_items = document.querySelector('#all_items')
+  list_of_all_items.insertAdjacentHTML('beforeend', li_item)
+  document.querySelector("#user_input").value = "";
+
+
+
+
+  list_of_all_items.addEventListener("click", (e) => {
+    console.log(e.target);
+    // if( list_of_all_items.style.textDecoration != "line-through"){
+    //   list_of_all_items.style.textDecoration = "line-through";
+    // }
+   
+    // else{
+    //   list_of_all_items.style.textDecoration = "none";
+    // }
+    
   })
 
-   document.querySelector("#all_items").appendChild(item);
-  // document.querySelector('#unique_id').appendChild(div_item);
- 
-  document.querySelector("#all_items").appendChild(img1);
-  document.querySelector("#all_items").appendChild(img2);
-  document.querySelector("#all_items").appendChild(img3);
-  document.querySelector("#user_input").value = "";
+
+
+
 }
 
 
+  
 
+ 
