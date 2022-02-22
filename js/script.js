9adf6d9e86e0ff49172206dfbf47a958a5c969b6
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2022-02-22 20:41:15 +0200

    fail with buttons

--- js/script.js
+++ js/script.js
@@ -127,7 +127,7 @@ addItem = () => {
 
     }
   
-    const li_item = `<li class = ${imageObject.li_contains}>- ${user_insert} <div class = ${imageObject.div_class}><img class = ${imageObject.class_bin} src = ${imageObject.src_bin}> <img class = ${imageObject.class_bin} src = ${imageObject.money_src}> <img class = ${imageObject.class_bin} src = ${imageObject.bag_src}></div></li>`
+    const li_item = `<li class = ${imageObject.li_contains}>- ${user_insert} <div class = ${imageObject.div_class}><img class = ${imageObject.class_bin} src = ${imageObject.src_bin}> <img class = ${imageObject.class_bin} > <img class = ${imageObject.class_bin} src = ${imageObject.bag_src}></div></li>`
     
 
 
@@ -135,20 +135,20 @@ addItem = () => {
   //   const item = document.createElement("li")
   //   item.id = "li_item_list";
 
-    const img1 = document.createElement("img")
-  img1.src = './images/bin.png'
-  img1.className = "item_img_of_list1"
-  img1.id = "first_img_in_list"
+  //   const img1 = document.createElement("img")
+  // img1.src = './images/bin.png'
+  // img1.className = "item_img_of_list1"
+  // img1.id = "first_img_in_list"
 
-  const img2 = document.createElement("img")
-  img2.src = './images/money.png'
-  img2.className = "item_img_of_list2"
-  img2.id = "second_img_in_list"
+  // const img2 = document.createElement("img")
+  // img2.src = './images/money.png'
+  // img2.className = "item_img_of_list2"
+  // img2.id = "second_img_in_list"
 
-  const img3 = document.createElement("img")
-  img3.src = './images/shopping-bag.png'
-  img3.className = "item_img_of_list3"
-  img3.id = "third_img_in_list"
+  // const img3 = document.createElement("img")
+  // img3.src = './images/shopping-bag.png'
+  // img3.className = "item_img_of_list3"
+  // img3.id = "third_img_in_list"
 
 
 // document.querySelector("#first_img_in_list").addEventListener("click", () => {
@@ -166,14 +166,14 @@ addItem = () => {
 
 
   list_of_all_items.addEventListener("click", (e) => {
-    console.log(e.target);
-    // if( list_of_all_items.style.textDecoration != "line-through"){
-    //   list_of_all_items.style.textDecoration = "line-through";
-    // }
+    console.log(e);
+    // if( e.target.style.textDecoration != "line-through")
+    //   e.target.style.textDecoration = "line-through";
+    
    
-    // else{
-    //   list_of_all_items.style.textDecoration = "none";
-    // }
+    // else
+    //   e.target.style.textDecoration = "none";
+    
     
   })
 
---
+++ js/switch_themes.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector("#theme-button");
+const link = document.querySelector("#theme-link");
+
+// btn.addEventListener("click", ChangeTheme);
+
+function ChangeTheme()
+{
+    const lightTheme = "css/light.css";
+    const darkTheme = "css/dark.css";
+
+    let currentTheme = link.getAttribute("href");
+    let theme = "";
+
+    if(currentTheme == lightTheme)
+    {
+   	 currentTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currentTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currentTheme);
+
+    
+}
