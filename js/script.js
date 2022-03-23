8fcdb07e8b7ef4fddc2d2a6dfe313b0302cbf63d
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2022-03-23 18:48:29 +0200

    qq

--- js/script.js
+++ js/script.js
@@ -1,124 +1,7 @@
-// document.querySelector("#pencil").addEventListener("click", () => {
-//   document.querySelector("#all_items").textContent = "";
-// })
-
-// document.querySelector("#user_input").addEventListener("keydown", (event) => {
-//   if(event.key == "Enter")
-//     addItem();
-// });
-
-// addItem = () => {
-//   const div_item = document.createElement("div")
-  
-
-
-
-//   const item = document.createElement("li")
-//   item.id = "li_item_list";
-
-//   const img1 = document.createElement("img")
-// img1.src = './images/bin.png'
-// img1.className = "item_img_of_list1"
-// img1.id = "first_img_in_list"
-
-// const img2 = document.createElement("img")
-// img2.src = './images/money.png'
-// img2.className = "item_img_of_list2"
-// img2.id = "second_img_in_list"
-
-// const img3 = document.createElement("img")
-// img3.src = './images/shopping-bag.png'
-// img3.className = "item_img_of_list3"
-// img3.id = "third_img_in_list"
-
-
-// // document.querySelector("#first_img_in_list").addEventListener("click", () => {
-// //   document.querySelector("#all_items").textContent = "";
-// // })
-
-
-
-  
-  
-//   item.textContent =   "- " + document.querySelector("#user_input").value;
- 
-  
-//   item.addEventListener("click", () => {
-//     if(item.style.textDecoration != "line-through")
-//       item.style.textDecoration = "line-through";
-//     else
-//       item.style.textDecoration = "none";
-//   })
-
-//    document.querySelector("#all_items").appendChild(item);
-//   // document.querySelector('#unique_id').appendChild(div_item);
- 
-//   document.querySelector("#all_items").appendChild(img1);
-//   document.querySelector("#all_items").appendChild(img2);
-//   document.querySelector("#all_items").appendChild(img3);
-//   document.querySelector("#user_input").value = "";
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.querySelector("#pencil").addEventListener("click", () => {
-//   document.querySelector("#all_items").textContent = "";
-// })
-
-// document.querySelector("#user_input").addEventListener("keydown", (event) => {
-//   if(event.key == "Enter")
-//     addItem();
-// });
-
-// addItem = () => {
-//   const user_insert = document.querySelector('#user_input').value
-// }
-
-
-
-    
-
-
-
-
-
-    const imageObject = {
+const imageObject = {
       src_bin: './images/bin.png',
       class_bin: 'item_img_of_list1',
+      class_bag: 'item_img_of_list2',
       li_contains: 'list_contains',
       div_class: 'div_class',
       money_src: './images/money.png',
@@ -126,72 +9,12 @@
       class_input: 'input_amount',
 
     }
-  
-    // const li_item = `<li class = ${imageObject.li_contains}>- ${user_insert} <div class = ${imageObject.div_class}><img class = ${imageObject.class_bin} src = ${imageObject.bag_src}> <img class = ${imageObject.class_bin} > <img class = ${imageObject.class_bin} src = ${imageObject.src_bin}></div></li>`
-    // const key_with_input = `<li class = ${imageObject.li_contains}>- ${user_insert} <div class = ${imageObject.div_class}><input name = weigth class = ${imageObject.class_input} > <img class = ${imageObject.class_bin} src = ${imageObject.src_bin}></div></li>`
 
-
-
-  //   const item = document.createElement("li")
-  //   item.id = "li_item_list";
-
-  //   const img1 = document.createElement("img")
-  // img1.src = './images/bin.png'
-  // img1.className = "item_img_of_list1"
-  // img1.id = "first_img_in_list"
-
-  // const img2 = document.createElement("img")
-  // img2.src = './images/money.png'
-  // img2.className = "item_img_of_list2"
-  // img2.id = "second_img_in_list"
-
-  // const img3 = document.createElement("img")
-  // img3.src = './images/shopping-bag.png'
-  // img3.className = "item_img_of_list3"
-  // img3.id = "third_img_in_list"
-
-
-// document.querySelector("#first_img_in_list").addEventListener("click", () => {
-//   document.querySelector("#all_items").textContent = "";
-// })
-
-
-
-  
-  // const list_of_all_items = document.querySelector('#all_items')
-  // list_of_all_items.insertAdjacentHTML('beforeend', li_item)
-  // document.querySelector("#user_input").value = "";
-
-
-
-  // list_of_all_items.addEventListener("click", (e) => {
-  //   const input_amount = document.createElement("input")
-  //   e.target.parentNode.appendChild(input_amount);
-    
-  //   if( e.target.style.textDecoration != "line-through")
-  //     e.target.style.textDecoration = "line-through";
-    
-   
-  //   else
-  //     e.target.style.textDecoration = "none";
-    
-    
-  // })
-
- 
-
-
-
-
-
-
-  // ====================================================================================
 const list_of_all_items = document.querySelector('#all_items')
 const inputRef = document.querySelector("#user_input")
 const item_back = document.querySelector('.itemInput')
 const item_span = document.querySelector('.span-bag')
 
-// const icon_bin = document.querySelector('.bin')
 
 
 
@@ -200,10 +23,7 @@ document.querySelector("#pencil").addEventListener("click", () => {
     document.querySelector("#all_items").textContent = "";
   })
   
-  // document.querySelector("#user_input").addEventListener("keydown", (event) => {
-  //   if(event.key == "Enter")
-  //   addItemToList();
-  // });
+
 
 const addItemToList = (e) => {
   const user_insert = inputRef.value;
@@ -222,10 +42,7 @@ const addItemToList = (e) => {
 
     }
     
-    //  if (user_insert === " "){
-    //   inputRef.value = ''
-          
-    //   }
+  
 
       
 
@@ -237,7 +54,7 @@ const addItemToList = (e) => {
     <span class=span-bag decoration></span>
     <div class = ${imageObject.div_class}>
       <img class = "${imageObject.class_bin} bin" src = ${imageObject.src_bin}> 
-      <img class = "${imageObject.class_bin} bag" src = ${imageObject.bag_src}>
+      <img class = "${imageObject.class_bag} bag" src = ${imageObject.bag_src}>
       <input type="text" maxlength="6"  class="itemInput nowiev" autofocus/>
     </div>
   </li>`
